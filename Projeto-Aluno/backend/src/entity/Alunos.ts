91ba35da0a7999b49971ec258a54434d0ca497a0
Pedro <pedrobento555@hotmail.com>
2021-09-29 21:19:16 -0300

    EX ALUNOS

---
+++ Projeto-Aluno/backend/src/entity/Alunos.ts
@@ -0,0 +1,27 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class Alunos{
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column()
+    name: string;
+    @Column()
+    age: number;
+    @Column()
+    ra: string;
+    @Column()
+    birth: Date;
+    @Column()
+    adress: string;
+    @Column({
+        default: true
+    })
+
+    registration: boolean;
+ 
+    @CreateDateColumn()
+    create_at: Date;
+    @UpdateDateColumn()
+    updated_at: Date;
+}
